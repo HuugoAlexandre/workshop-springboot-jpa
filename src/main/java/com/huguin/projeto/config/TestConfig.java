44d2f62501f90ff0328d28fc3607ef6b2173995f
HuugoAlexandre <huugoalexanndre@gmail.com>
2025-01-22 16:35:04 -0300

    Payment, one-to-one association

--- src/main/java/com/huguin/projeto/config/TestConfig.java
+++ src/main/java/com/huguin/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.huguin.projeto.entities.Category;
 import com.huguin.projeto.entities.Order;
 import com.huguin.projeto.entities.OrderItem;
+import com.huguin.projeto.entities.Payment;
 import com.huguin.projeto.entities.Product;
 import com.huguin.projeto.entities.User;
 import com.huguin.projeto.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
